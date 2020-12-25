c94768b0b6268be83d5e4a4f56901bf847a9eadd
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2020-12-25 11:07:13 -0700

    adding a test for r distribution

--- tests/test_mie.py
+++ tests/test_mie.py
@@ -16,7 +16,15 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(len(wave),len(qsca))
         self.assertEqual(len(wave),len(qback))
         self.assertEqual(len(wave),len(g))
-        
+    
+    def test_lognorm_eval(self):
+        wave = [0.5,1.3,2.5]
+        res = dust_mie.calc_mie.get_mie_coeff_distribution([0.5,1.3,2.5],r=0.5,material='Mg2SiO4')
+        self.assertTrue(np.allclose(res[0],[2.74404239, 2.88194858, 1.29673223],rtol=1e-4))
+        self.assertTrue(np.allclose(res[1],[2.74403313, 2.88189223, 1.29656406],rtol=1e-4))
+        self.assertTrue(np.allclose(res[2],[8.32053549, 1.82168508, 0.37357759],rtol=1e-4))
+        self.assertTrue(np.allclose(res[3],[0.58241075, 0.55545767, 0.37230144],rtol=1e-4))
+    
 
 if __name__ == '__main__':
     unittest.main()
